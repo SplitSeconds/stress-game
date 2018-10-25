2ece5324d56c5bebd29c96f75690308c785d8e02
Nele Luepkes <nluepkes@gmail.com>
2018-10-25 10:29:18 +0200

    failed at added difficulty

--- JS/main.js
+++ JS/main.js
@@ -80,8 +80,8 @@ function update() {
       if(character.collide(obstacles[i])){
          crashSound = new sound("../sound/crash.wav");
          crashSound.play();
-        //  character = new Player(ctx, "../img/Autsch.png");
-        playImage.src = "./img/Autsch.png"
+        
+         playImage.src = "./img/Autsch.png"
          
         setTimeout(()=> {
           ctx.font = "120px VT323"
@@ -177,21 +177,33 @@ document.onkeyup = function(e) {
 
 
 function createObstacle () {
-  if (frames % 140 === 0) {  
+  if (frames % 160 === 0) {  
     var x = 1200
     var minHeight = 120;
     var maxHeight = 190;
     var height = Math.floor(Math.random()*(maxHeight-minHeight+1)+minHeight);
-    minGap = 410;
+    minGap = 430;
     maxGap = 550;
     gap = Math.floor(Math.random()*(maxGap-minGap+1)+minGap);
-    
     obstacles.push(new Obstacle(ctx, 100, 150, '../img/Trashcan.png', x, height + gap)); 
-   
   }
   for (let i = 0; i < obstacles.length; i += 1) {
-    obstacles[i].x += -1;
+    obstacles[i].x += -1.5;
     obstacles[i].update();
+
+    // if (score <= 50){
+    //   obstacles[i].x += -3;
+    //   obstacles[i].update();
+    // } else if(score <=100){
+    //   obstacles[i].x += -4;
+    //   obstacles[i].update();
+    // } else if(score <=150){
+    //   obstacles[i].x += -6;
+    //   obstacles[i].update();
+    // } else {
+    //   obstacles[i].x += -8;
+    //   obstacles[i].update();
+    // }
   }
 }
 
@@ -201,7 +213,16 @@ function createCoffee () {
     coffees.push(new Coffee(ctx, 100, 150, '../img/coffee.png', x, 650)); 
   }
   for (let j = 0; j < coffees.length; j += 1) {
-    coffees[j].x += -1;
-    //coffeeCups[j].update();
+    coffees[j].x += -1.5;
+    
+    // if (score <= 50){
+    //   coffees[j].x += -3;
+    // } else if(score <=100){
+    //   coffees[j].x += -4;
+    // } else if(score <=150){
+    //   coffees[j].x += -6;
+    // } else {
+    //   coffees[j].x += -8;
+    // }
   }
 }
