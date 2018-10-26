ce754818e1dd445d433554fc75ed85169ed592b4
Zac Connelly <zconnelly13@gmail.com>
2018-10-26 11:11:46 +0200

    fix issue with background not loading without refresh

--- js/background.js
+++ js/background.js
@@ -7,21 +7,30 @@ class Background {
     this.img.src = url
     this.x = 0
     this.height = this.ctx.canvas.height
-    this.width = this.height*this.img.width/this.img.height
   }
   update() {
     this.x -= this.speed
-    if (this.x < -this.width) {
-      this.x += this.width
+    if (this.x < -this.width()) {
+      this.x += this.width()
     }
   }
   stop(){
     this.x = 0
-    
+
+  }
+  /*
+  /  width() has to be a function because when you initially load the image
+  /  `this.img.width` and `this.img.height` are both 0
+  /  this leads to a division by 0 and width gets set to `NaN`
+  /
+  /  Another soluton could be to only set the width after the image has loaded
+  */
+  width() {
+    return this.height*this.img.width/this.img.height;
   }
   draw() {
-    for (var i = 0; this.x+i*this.width < this.ctx.canvas.width; i++) {
-      this.ctx.drawImage(this.img,this.x+i*this.width,0,this.width,this.height)
+    for (var i = 0; this.x+i*this.width() < this.ctx.canvas.width; i++) {
+      this.ctx.drawImage(this.img,this.x+i*this.width(),0,this.width(),this.height)
     }
   }
 }
