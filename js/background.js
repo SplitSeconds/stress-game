198a929727a8bb2b4cabd3047989f28963835b81
Nele Luepkes <nluepkes@gmail.com>
2018-10-26 11:29:44 +0200

    finally fixed bg issues thx Zac

--- js/background.js
+++ js/background.js
@@ -1,4 +1,3 @@
-
 class Background {
   constructor(ctx, url, speed) {
     this.ctx = ctx
@@ -7,21 +6,49 @@ class Background {
     this.img.src = url
     this.x = 0
     this.height = this.ctx.canvas.height
-    this.width = this.height*this.img.width/this.img.height
   }
   update() {
     this.x -= this.speed
-    if (this.x < -this.width) {
-      this.x += this.width
+    if (this.x < -this.width()) {
+      this.x += this.width()
     }
   }
   stop(){
     this.x = 0
-    
   }
+  width() {
+    return this.height*this.img.width/this.img.height;
+  }	  
   draw() {
-    for (var i = 0; this.x+i*this.width < this.ctx.canvas.width; i++) {
-      this.ctx.drawImage(this.img,this.x+i*this.width,0,this.width,this.height)
+    for (var i = 0; this.x+i*this.width() < this.ctx.canvas.width; i++) {
+      this.ctx.drawImage(this.img,this.x+i*this.width(),0,this.width(),this.height)
     }
   }
 }
+
+// class Background {
+//   constructor(ctx, url, speed) {
+//     this.ctx = ctx
+//     this.speed = speed
+//     this.img = new Image()
+//     this.img.src = url
+//     this.x = 0
+//     this.height = this.ctx.canvas.height
+//     this.width = this.height*this.img.width/this.img.height
+//   }
+//   update() {
+//     this.x -= this.speed
+//     if (this.x < -this.width) {
+//       this.x += this.width
+//     }
+//   }
+//   stop(){
+//     this.x = 0
+    
+//   }
+//   draw() {
+//     for (var i = 0; this.x+i*this.width < this.ctx.canvas.width; i++) {
+//       this.ctx.drawImage(this.img,this.x+i*this.width,0,this.width,this.height)
+//     }
+//   }
+// }
